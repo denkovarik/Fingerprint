4d614aa442513507e07256b5a682e1a2b89dbf52
Dennis <denkovarik123@gmail.com>
2024-01-03 06:34:23 -0700

    Updated install script

--- Graph/install.sh
+++ Graph/install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # Update and upgrade the package lists
-sudo apt-get update
-sudo apt-get -y upgrade
+apt-get update
+apt-get -y upgrade
 
 # Install Python package 'graphviz'
-sudo pip install graphviz
+pip3 install graphviz
 
 # Install the graphviz package
-sudo apt-get -y install graphviz
+apt-get -y install graphviz
 
 # Install the xdg-utils package
-sudo apt-get -y install xdg-utils
+apt-get -y install xdg-utils
 
+pip3 install ipython
