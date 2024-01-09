015a7405b1d6a6a858806feb4a872cd1cf9a7987
Dennis <denkovarik123@gmail.com>
2024-01-09 07:28:52 -0700

    Wrote test to test writing and reading graph dict to file

--- Graph/testing/graphTests.py
+++ Graph/testing/graphTests.py
@@ -4,24 +4,29 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 from classes.Nodes import *
-from classes.Graph import *
+from classes.Graph import Graph
 
 
 class graphTests(unittest.TestCase):
     """
-    Runs all tests.
+    Runs graph tests.
     """ 
         
     def testExecution(self):
         """
-        Tests the ability of the allTests class to run a test.
+        Tests the ability of the graphTests class to run a test.
         
-        :param self: An instance of the allTests class.
+        :param self: An instance of the graphTests class.
         """
         self.assertTrue(True)
 
 
     def testConstructGraph(self):
+        """
+        Tests the ability of the Graph class member function 'construct'. 
+        
+        :param self: An instance of the graphTests class.
+        """
         graph = Graph()
         self.assertTrue(isinstance(graph, Graph))
         self.assertTrue(graph.graph == {})
@@ -30,11 +35,65 @@ class graphTests(unittest.TestCase):
 
     
     def testWriteGraph2File(self):
+        """
+        Tests the ability of the Graph class to write the dictionary of it's 
+        graph to file.
+        
+        :param self: An instance of the graphTests class.
+        """
         graph = Graph()
         self.assertTrue(isinstance(graph, Graph))
         self.assertTrue(graph.graph == {})
-        graph.construct()
+
+        # Construct a simple graph example
+        inputNode = InputNode(numChannels=3)
+        normNode = NormalizationNode('normNode', NormalizationType.BATCH_NORM)
+        convNode = ConvolutionalNode(name='convNode1', kernelSize=3, maxNumInputChannels=16, numOutputChannels=4)
+        
+        graph.graph[inputNode.name] = {'node': inputNode, 'edges': [normNode.name]}
+        graph.graph[normNode.name] = {'node': normNode, 'edges': [convNode.name]}
+        graph.graph[convNode.name] = {'node': convNode, 'edges': []}
+
         self.assertTrue(not graph.graph == {})
+
+        graphFilepath = currentdir + 'Temp/graphTest.txt'
+        graphFilepath = os.path.join(currentdir, 'Temp', 'graphTest.txt')
+        graph.writeGraph2File(graphFilepath)
+
+        self.assertTrue(os.path.exists(graphFilepath))
+
+        if os.path.exists(graphFilepath):
+            os.remove(graphFilepath)
+        else:
+            self.assertTrue(False, 'Graph file ' + graphFilepath + ' does not exist')
+
+    
+    def testReadGraph2(self):
+        """
+        Tests the ability of the Graph class to read a graph from file.
+        
+        :param self: An instance of the graphTests class.
+        """
+        graph = Graph()
+        self.assertTrue(isinstance(graph, Graph))
+        self.assertTrue(graph.graph == {})
+
+        # Construct a simple graph example to test the read
+        inputNode = InputNode(numChannels=3)
+        normNode = NormalizationNode('normNode', NormalizationType.BATCH_NORM)
+        convNode = ConvolutionalNode(name='convNode1', kernelSize=3, maxNumInputChannels=16, numOutputChannels=4)
+        
+        graph.graph[inputNode.name] = {'node': inputNode, 'edges': [normNode.name]}
+        graph.graph[normNode.name] = {'node': normNode, 'edges': [convNode.name]}
+        graph.graph[convNode.name] = {'node': convNode, 'edges': []}
+
+        self.assertTrue(not graph.graph == {})
+
+        graphFilepath = currentdir + 'Temp/graphTest.txt'
+        graphFilepath = os.path.join(currentdir, 'Temp', 'graphTest.txt')
+        graph.writeGraph2File(graphFilepath) 
+
+        # Test reading the graph from 'testing/TestFiles/graphTest.txt' 
  
         
 if __name__ == '__main__':
