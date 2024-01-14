4c611b341e33d3c0c775d5fcac30c29ef7d6647a
Dennis <denkovarik123@gmail.com>
2024-01-14 12:57:31 -0700

    Created method to clear the screen

--- Graph/utils.py
+++ Graph/utils.py
@@ -1,4 +1,14 @@
 from IPython import get_ipython
+import os
+
+
+def clearScreen():
+    # For Windows
+    if os.name == 'nt':
+        _ = os.system('cls')
+    # For Mac and Linux (os.name: 'posix')
+    else:
+        _ = os.system('clear')
 
 
 def is_running_in_jupyter_notebook():
