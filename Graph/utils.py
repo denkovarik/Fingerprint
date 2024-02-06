71eb038d7a9921a2fb202b3af638d202e41dc928
Dennis <denkovarik123@gmail.com>
2024-02-06 06:16:55 -0700

    Implemented method to unpickle cifar10 test batch

--- Graph/utils.py
+++ Graph/utils.py
@@ -50,3 +50,11 @@ def renderGraph(graph, renderFP):
         os.remove(fp)
     if os.path.exists(imagePath):
         os.remove(imagePath)
+
+
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo: 
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
