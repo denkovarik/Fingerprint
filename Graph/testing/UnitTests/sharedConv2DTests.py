bf16a105a2966ee8cf42c15b4233e6d1ce87bf0b
Dennis <denkovarik123@gmail.com>
2024-02-12 07:18:36 -0700

    Started testing for SharedConv2D method 'calcOutSize()'

--- Graph/testing/UnitTests/sharedConv2DTests.py
+++ Graph/testing/UnitTests/sharedConv2DTests.py
@@ -37,6 +37,34 @@ class sharedConv2DTests(unittest.TestCase):
         self.assertTrue(isinstance(sharedConv, SharedConv2D))
 
 
+    def testCalcOutputSize(self):
+        """
+        Tests method calcOutSize of the SharedConv2DTests class
+
+        :param self: An instance of the sharedConv2DTests class.
+        """
+        conv2d = nn.Conv2d(3, 8, 3)
+        sharedConv2d = SharedConv2D(3, 3, 8)
+        # Get test batch
+        testBatchPath = os.path.join(currentdir, 'TestFiles/cifar10_test_batch_pickle')
+        self.assertTrue(testBatchPath)
+        testBatch = unpickle(testBatchPath)
+        imgData = testBatch[b'data'][:4]
+        batch = imgData.reshape(4, 3, 32, 32)
+        tensorData = torch.tensor(batch, dtype=torch.float32)
+        tensorData = torch.tensor(testBatch[b'data'][:4], dtype=torch.float32).reshape(4, 3, 32, 32)
+        # Forward prop
+        calcOutputSize = sharedConv2d.calcOutSize(inputHeight=tensorData.shape[2],
+                                                  inputWidth=tensorData.shape[3])
+        outConv2d = conv2d(tensorData)
+        outSharedConv2d = sharedConv2d(tensorData, 3, 8)
+        self.assertTrue(calcOutputSize[0] == outConv2d.shape[2])
+        self.assertTrue(calcOutputSize[1] == outConv2d.shape[3])
+        self.assertTrue(calcOutputSize[0] == outSharedConv2d.shape[2])
+        self.assertTrue(calcOutputSize[1] == outSharedConv2d.shape[3])
+
+
+
     def testForwardPass(self):
         """
         Tests the forward pass for the SharedConv2DTests class.
@@ -70,7 +98,6 @@ class sharedConv2DTests(unittest.TestCase):
         testBatchPath = os.path.join(currentdir, 'TestFiles/cifar10_test_batch_pickle')
         self.assertTrue(testBatchPath)
         testBatch = unpickle(testBatchPath)
-        # Test pytorch layers on images from test batch
         imgData = testBatch[b'data'][:4]
         batch = imgData.reshape(4, 3, 32, 32)
         tensorData = torch.tensor(batch, dtype=torch.float32)
