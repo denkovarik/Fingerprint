31c0e093ade5544d4fccab28b126a82c1b05fc55
Dennis <denkovarik123@gmail.com>
2024-02-15 06:10:31 -0700

    Added more tests for SharedConv2d method 'calcOutSize()'

--- Graph/testing/UnitTests/sharedConv2dTests.py
+++ Graph/testing/UnitTests/sharedConv2dTests.py
@@ -51,12 +51,58 @@ class sharedConv2DTests(unittest.TestCase):
         batch = imgData.reshape(4, 3, 32, 32)
         tensorData = torch.tensor(batch, dtype=torch.float32)
         tensorData = torch.tensor(testBatch[b'data'][:4], dtype=torch.float32).reshape(4, 3, 32, 32)
-        # Kernel Size 3x3
-        conv2d = nn.Conv2d(3, 8, 3)
-        sharedConv2d = SharedConv2d(3, 8, 3)
-        # Forward prop
+        
+        # Test 1: Kernel Size 3x3
+        conv2d = nn.Conv2d(3, 8,kernel_size=3)
+        sharedConv2d = SharedConv2d(3, 8, kernel_size=3)
+        # Calculate what the output size should be
         calcOutputSize = sharedConv2d.calcOutSize(inputHeight=tensorData.shape[2],
                                                   inputWidth=tensorData.shape[3])
+        # Forward prop
+        outConv2d = conv2d(tensorData)
+        outSharedConv2d = sharedConv2d(tensorData, 3, 8)
+        self.assertTrue(calcOutputSize[0] == outConv2d.shape[2])
+        self.assertTrue(calcOutputSize[1] == outConv2d.shape[3])
+        self.assertTrue(calcOutputSize[0] == outSharedConv2d.shape[2])
+        self.assertTrue(calcOutputSize[1] == outSharedConv2d.shape[3])
+
+        # Test 2: Kernel Size 5x5
+        conv2d = nn.Conv2d(3, 16, kernel_size=5)
+        sharedConv2d = SharedConv2d(3, 16, kernel_size=5)
+        # Calculate what the output size should be
+        calcOutputSize = sharedConv2d.calcOutSize(inputHeight=tensorData.shape[2],
+                                                  inputWidth=tensorData.shape[3])
+        # Forward prop
+        outConv2d = conv2d(tensorData)
+        outSharedConv2d = sharedConv2d(tensorData, 3, 16)
+        self.assertTrue(calcOutputSize[0] == outConv2d.shape[2])
+        self.assertTrue(calcOutputSize[1] == outConv2d.shape[3])
+        self.assertTrue(calcOutputSize[0] == outSharedConv2d.shape[2])
+        self.assertTrue(calcOutputSize[1] == outSharedConv2d.shape[3])
+
+        # Test 3: Shared Conv2d ouput number of Channels less than max 
+        #         Kernel Size 3x3
+        conv2d = nn.Conv2d(3, 8,kernel_size=3)
+        sharedConv2d = SharedConv2d(3, 16, kernel_size=3)
+        # Calculate what the output size should be
+        calcOutputSize = sharedConv2d.calcOutSize(inputHeight=tensorData.shape[2],
+                                                  inputWidth=tensorData.shape[3])
+        # Forward prop
+        outConv2d = conv2d(tensorData)
+        outSharedConv2d = sharedConv2d(tensorData, 3, 8)
+        self.assertTrue(calcOutputSize[0] == outConv2d.shape[2])
+        self.assertTrue(calcOutputSize[1] == outConv2d.shape[3])
+        self.assertTrue(calcOutputSize[0] == outSharedConv2d.shape[2])
+        self.assertTrue(calcOutputSize[1] == outSharedConv2d.shape[3])
+
+        # Test 4: Shared Conv2d ouput number of Channels less than Conv2d 
+        #         output channels Kernel Size 5x5
+        conv2d = nn.Conv2d(3, 64,kernel_size=5)
+        sharedConv2d = SharedConv2d(3, 256, kernel_size=5)
+        # Calculate what the output size should be
+        calcOutputSize = sharedConv2d.calcOutSize(inputHeight=tensorData.shape[2],
+                                                  inputWidth=tensorData.shape[3])
+        # Forward prop
         outConv2d = conv2d(tensorData)
         outSharedConv2d = sharedConv2d(tensorData, 3, 8)
         self.assertTrue(calcOutputSize[0] == outConv2d.shape[2])
