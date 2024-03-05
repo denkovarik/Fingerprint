1d96601254a2df2ee3e5ea058040b483499d3ffe
Dennis <denkovarik123@gmail.com>
2024-03-05 07:01:20 -0700

    Overloaded to string function and changed paramter names

--- Graph/classes/SharedLinear.py
+++ Graph/classes/SharedLinear.py
@@ -5,21 +5,21 @@ import torch.nn.init as init
 import math
 
 class SharedLinear(nn.Module):
-    def __init__(self, maxInFeatures, maxOutFeatures, useBatchnorm=False, dropoutRate=0.0):
+    def __init__(self, max_in_features, max_out_features, useBatchnorm=False, dropoutRate=0.0):
         super(SharedLinear, self).__init__()
-        self.maxInFeatures = maxInFeatures
-        self.maxOutFeatures = maxOutFeatures
+        self.maxInFeatures = max_in_features
+        self.maxOutFeatures = max_out_features
         self.useBatchnorm = useBatchnorm
         self.dropoutRate = dropoutRate
 
         # Initialize the weights and biases for the maximum configuration
-        self.weight = nn.Parameter(torch.Tensor(maxOutFeatures, maxInFeatures))
-        self.bias = nn.Parameter(torch.Tensor(maxOutFeatures))
+        self.weight = nn.Parameter(torch.Tensor(self.maxOutFeatures, self.maxInFeatures))
+        self.bias = nn.Parameter(torch.Tensor(self.maxOutFeatures))
 
         # Initialize weights using Kaiming (He) initialization
         init.kaiming_uniform_(self.weight, a=math.sqrt(5))
         if self.bias is not None:
-            fan_in = maxInFeatures  # Number of input features
+            fan_in = self.maxInFeatures  # Number of input features
             bound = 1 / math.sqrt(fan_in)
             init.uniform_(self.bias, -bound, bound)
 
@@ -29,6 +29,15 @@ class SharedLinear(nn.Module):
 
         # Dropout layer
         #self.dropout = nn.Dropout(p=dropoutRate)
+
+
+    def __str__(self):
+        strRep = (f"SharedLinear("
+                  f"max_in_features={self.maxInFeatures}, "
+                  f"max_out_features={self.maxOutFeatures}"
+                  f")")
+        return strRep
+
 
     def forward(self, x, inChannels, outChannels):
         # Dynamically select the subset of weights and biases
