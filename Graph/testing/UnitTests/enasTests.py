9bcead07a98582161a30e351056e14b430720c76
Dennis <denkovarik123@gmail.com>
2024-04-21 16:12:37 -0600

    Create unit test class for ENAS class

---
+++ Graph/testing/UnitTests/enasTests.py
@@ -0,0 +1,30 @@
+import unittest
+import os, io, sys, inspect
+from unittest.mock import patch
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+graphdir = os.path.dirname(parentdir)
+sys.path.insert(0, graphdir)
+from classes.Nodes import *
+from classes.Graph import Graph
+from utils import *
+import torch
+import torch.nn as nn
+    
+
+class enasTests(unittest.TestCase):
+    """
+    Runs graph tests.
+    """ 
+        
+    def testExecution(self):
+        """
+        Tests the ability of the graphTests class to run a test.
+        
+        :param self: An instance of the graphTests class.
+        """
+        self.assertTrue(True)
+ 
+        
+if __name__ == '__main__':
+    unittest.main()
