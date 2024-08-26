9ec240dc99df282a1f2274435f39bff190c39034
Dennis <denkovarik123@gmail.com>
2024-08-26 06:24:43 -0600

    Moved script into setup folder

---
+++ Graph/setup/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Update and upgrade the package lists
+apt-get update
+apt-get -y upgrade
+
+# Install packages to create and render graphs
+pip3 install graphviz
+apt-get -y install graphviz
+apt-get install feh
+
+# Install the xdg-utils package
+apt-get -y install xdg-utils
+
+pip3 install ipython
+
+# Install PyTorch
+pip install torch torchvision torchaudio
+
+pip install tqdm
