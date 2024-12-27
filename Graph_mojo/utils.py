5daa5dd733742f2d769123968b72fdac99c0c7e1
Dennis <denkovarik123@gmail.com>
2024-12-27 15:19:19 -0500

    Added utils file to help with testing

---
+++ Graph_mojo/utils.py
@@ -0,0 +1,67 @@
+from IPython import get_ipython
+import os
+
+
+def clearScreen():
+    # For Windows
+    if os.name == 'nt':
+        _ = os.system('cls')
+    # For Mac and Linux (os.name: 'posix')
+    else:
+        _ = os.system('clear')
+
+
+def ensureDirpathExists(dirpath):
+    if dirpath and not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+
+
+def ensureFilepathExists(filepath):
+    dirpath = os.path.dirname(filepath)
+    ensureDirpathExists(dirpath)
+
+
+def isRunningInJupyterNotebook():
+    try:
+        if 'IPKernelApp' not in get_ipython().config:  # Check if IPython kernel is running
+            return False
+    except (ImportError, AttributeError):
+        return False
+
+    return True
+
+
+def renderGraph(graph, renderFP):
+    """
+    Helper function for rendering and displaying the graph
+
+    :param graph: Instance of the Graph class to render
+    :param renderFP: Filepath to temp dir
+    """
+    imagePath = graph.render(renderFP)        
+    if isRunningInJupyterNotebook():
+        display(Image(filename=imagePath))
+    else:
+        os.system(f'feh {imagePath}')
+
+    # Remove files used to render graph
+    fp = imagePath[:imagePath.rfind('.')]
+    if os.path.exists(fp):
+        os.remove(fp)
+    if os.path.exists(imagePath):
+        os.remove(imagePath)
+
+
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo: 
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+
+def unpickle_test_data(file, ind):
+    import pickle
+    with open(file, 'rb') as fo: 
+        dict = pickle.load(fo, encoding='bytes')
+    imgData = dict[b'data'][:ind]
+    return imgData
