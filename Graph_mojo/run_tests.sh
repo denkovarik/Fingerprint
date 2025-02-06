ab18644011c8bda8435236d78d8adc7364cb43a8
Dennis <denkovarik123@gmail.com>
2025-02-06 10:58:55 -0500

    Modified how tests are run

--- Graph_mojo/run_tests.sh
+++ Graph_mojo/run_tests.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 # Runs all tests
-mojo test -I src testing
+# This is breaking for some reason
+#mojo test -I src testing
+
+# Run Unit tests
+mojo test -I src testing/UnitTests
+
+# Run Functional tests
+mojo test -I src testing/FunctionalTests
